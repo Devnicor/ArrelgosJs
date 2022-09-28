3c632ce45d2bc28a92379abec09e8bdc29541323
Devnicor <110273923+Devnicor@users.noreply.github.com>
2022-09-27 22:55:41 -0500

    Actualizacion

---
+++ Arreglos.js
@@ -0,0 +1,14 @@
+//Demostracion basica de arreglos:
+var vec = [];
+console.log(vec);
+vec[0] = 100;
+console.log(vec);
+vec.push([1,2,3]);
+console.log(vec);
+var vector = [];
+for (let i = 0; i < 20; i++){
+    vector[i] = Math.round((Math.random())*100);
+}
+console.log(vector);
+
+//Operaciones con los elementos del arreglo:
